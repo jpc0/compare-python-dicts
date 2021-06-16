ded8cba8c6267e711b04ca07aaecf5095cebf6aa
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2021-06-16 17:37:49 +0200

    Added compare_dicts which was listed as a submodule previously

---
+++ compare_dicts/compare_dict.py
@@ -0,0 +1,37 @@
+class DictCompare:
+    @staticmethod
+    def dict_in_dict(dict1, dict2):
+        """ This fuction ensure that all the keys in dict 1 are present in dict 2
+            and are of the same type.
+
+            If the type of any value is a dict, it recursively checks those dicts 
+        """
+        for i in dict1.keys():
+            if i in dict2.keys():
+                if type(dict1[i]) == type(dict2[i]):
+                    pass
+                else:
+                    return False
+                if type(dict1[i]) == dict:
+                    DictCompare.dict_in_dict(dict1[i], dict2[i])
+                elif type(dict1[i]) == list:
+                    DictCompare.list_in_list(dict1[i], dict2[i])
+            else:
+                return False
+        return True
+
+    @staticmethod
+    def list_in_list(list1, list2):
+        for i in list1:
+            if i in list2:
+                index = list2.index(i)
+                if type(i) == type(list2[index]):
+                    pass
+                else:
+                    return False
+                if type(i) == dict:
+                    DictCompare.dict_in_dict(i, list2[index])
+                elif type(i) == list:
+                    DictCompare.list_in_list(i, list2[index])
+            else:
+                return False
