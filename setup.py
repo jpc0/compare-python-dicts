71f11958c647004f0c22a2d0520b33c315b2e383
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2021-06-16 17:00:38 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='compare-python-dicts',
+version='0.1',
+description='Library for comparing python dictionarys',
+url='#',
+author='Jean-Pierre Coetzee',
+author_email='jeanpierrec19@gmail.com',
+license='MIT',
+packages=['compare_dicts'],
+zip_safe=False)
